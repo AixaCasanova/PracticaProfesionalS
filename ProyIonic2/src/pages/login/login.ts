26db521e6e07cdf47bd84b1b5adaad12414b6d4c
Aixa <aixacasanova@gmail.com>
2017-03-31 22:25:28 -0300

    se agrega login
    se agrega login

---
+++ ProyIonic2/src/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'login.html'
+})
+
+export class login 
+{
+
+    constructor(public navCtrl: NavController) 
+    {
+
+    }
+ 
+    Unusuario = {};
+    
+
+    loguear()
+    {
+      
+        console.log(this.Unusuario['usuario']);
+        alert("usuario:"+this.Unusuario['usuario']+ "  password:"+this.Unusuario['password']);
+    }
+ 
+}
+
+export class persona 
+{
+
+  usuario:string="";
+  password:string="";
+  constructor(public navCtrl: NavController) {
+
+  }}
